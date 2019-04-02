6aeb5ac8f6714b58b0767dc83f7ab384a123fdb2
Jinawong <jokjack26172@gmail.com>
2019-04-02 16:55:22 +0700

    update swarm config

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-'use strict'
-const hapi = require('hapi');
-const path = require('path');
-require('dotenv').config({
-    path: path.join(__dirname, '../.env')
+"use strict";
+const hapi = require("hapi");
+const path = require("path");
+require("dotenv").config({
+  path: path.join(__dirname, "../.env")
 });
 
-const router = require('./module/router');
+const router = require("./modules/router");
 
 const server = hapi.server({
-    port: process.env.PORT,
-    host: process.env.HOST,
-    routes: {
-        cors: {
-            origin: ['*'],
-        }
-    },
-    debug: {
-        log: ['*']
+  port: process.env.PORT,
+  host: process.env.HOST,
+  routes: {
+    cors: {
+      origin: ["*"]
     }
+  },
+  debug: {
+    log: ["*"]
+  }
 });
 
-const init = async () => {  
-    await router(server);
-    await server.start();
-    console.log(`Server running at : ${server.info.uri}`);
+const init = async () => {
+  await router(server);
+  await server.start();
+  console.log(`Server running at : ${server.info.uri}`);
 };
 
-
-server.events.on('log', (event, tags) => {
-    console.log(tags);
+server.events.on("log", (event, tags) => {
+  console.log(tags);
 });
 
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+process.on("unhandledRejection", err => {
+  console.log(err);
+  process.exit(1);
 });
 
 init();
